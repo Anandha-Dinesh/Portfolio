d0599a3602e838f7ed210df94d6465b45a159753
Anandha-Dinesh <codemon57@gmail.com>
2024-09-22 15:29:26 +0530

    Added project component

--- src/app/components/project_tile.js
+++ src/app/components/project_tile.js
@@ -1,10 +1,31 @@
-import Link from "next/link";
-const ProjectTile = () => {
-	return (
-		<>
-			<div className="w-52 h-48 border-solid border-2 border-red-100 m-4"></div>
-		</>
-	);
+import React, { useState } from "react";
+
+const ProjectTile = ({ title, description, url }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleClick = () => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
+
+  return (
+    <div
+      className="relative w-64 h-64  bg-gray-200 rounded-lg shadow-md overflow-hidden cursor-pointer transition-transform duration-300 transform hover:scale-105"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      onClick={handleClick}
+    >
+      <div className="absolute text-gray-100 inset-0 flex items-center justify-center p-4">
+        <h3 className="text-xl  font-bold text-center text-gray-800 text-pretty">
+          {"result scrapper"}
+        </h3>
+      </div>
+      {isHovered && (
+        <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center p-4 text-pretty">
+          <p className="text-white text-center">{"description"}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ProjectTile;

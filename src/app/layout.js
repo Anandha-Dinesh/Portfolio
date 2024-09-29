e9f375d8f6f8ec5c965d5304de5ef24a8a35cd42
Anandha-Dinesh <codemon57@gmail.com>
2024-09-29 20:11:43 +0530

    Added vercel Insights and analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,21 @@
-import {Inter} from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-	title: "Portfolio",
-	description: "The site where you can know me",
+  title: "Portfolio",
+  description: "The site where you can know me",
 };
 
-export default function RootLayout({children}) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+      <SpeedInsights />
+      <Analytics />
+    </html>
+  );
 }

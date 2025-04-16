095d9e95fdfd0d050ff059967d8ed149ffde8506
Anandha Dinesh <116005002+Anandha-Dinesh@users.noreply.github.com>
2025-04-16 22:36:33 +0530

    Update info.js

--- public/images/info.js
+++ public/images/info.js
@@ -2,7 +2,7 @@ export const infos = {
   personal_info: {
     name: "Anandha Dinesh",
     email: "anandhadinesh80@gmail.com",
-    intrests: ["Developer", "Analyst", "Gamer"],
+    intrests: ["Developer", "Analyst", "Freelancer", "Gamer"],
   },
   social: {
     github: "https://github.com/Anandha-Dinesh",
